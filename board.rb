6fa6f827c39eaf5d82214bb3b4d057d5a9863988
App Academy Student <student@appacademy.io>
2014-09-18 14:03:56 -0400

    implement jump and slide functionality for regular pieces

--- board.rb
+++ board.rb
@@ -2,7 +2,8 @@ require_relative 'piece.rb'
 
 class Board
 
-  attr_accessor: :board
+  attr_accessor :board
+
   def initialize
     @board = Array.new(8){Array.new(8)}
     self.set_up_board
@@ -11,22 +12,59 @@ class Board
 
   def set_up_board
     #top pieces
-    board[0...3].each_with_index do |row, y|
-      row.each_with_index do |square, x|
-        Piece.new(:light, [x, y], self) if X + Y % 2 == 0
+    (0..2).each do |y|
+      (0..7).each do |x|
+        # p "light x #{x}, y #{y}"
+        Piece.new(:l, [x,y], self) if (x + y + 1) % 2 == 0
       end
     end
 
     #bottom pieces
-    board[5..7].each_with_index do |row, y|
-      row.each_with_index do |square, x|
-        y += 5
-        Piece.new(:dark, [x, y], self) if X + Y % 2 == 0
+    (5..7).each do |y|
+      (0..7).each do |x|
+        # p "dark x #{x}, y #{y}"
+        Piece.new(:d, [x,y], self) if (x + y + 1) % 2 == 0
       end
     end
   end
 
+  def add_piece(piece, pos)
+    self[pos]= piece
+  end
 
+  def valid_pos?(pos)
+    (0..7).include?(pos[0]) && (0..7).include?(pos[1])
+  end
+
+  def [](pos)
+    x = pos[0]
+    y = pos[1]
+
+    self.board[y][x]
+  end
+
+  def []=(pos, piece)
+    #p pos
+    #p piece.color
+    x = pos[0]
+    y = pos[1]
+
+    self.board[y][x] = piece
+  end
+
+  def inspect
+    p "board"
+  end
+
+  def render
+    puts "  " + (0..7).to_a.join("   ")
+    a = self.board.map.with_index do |row, row_indx|
+          "#{row_indx}" + row.map do |square|
+            square ? " #{square.color} " : " * "
+          end.join(" ")
+        end.join("\n")
+        puts a
+  end
 
 
 end
--- piece.rb
+++ piece.rb
@@ -1,13 +1,83 @@
 class Piece
-attr_reader: :color, :pos, :board
-  def initialize
+attr_reader :color, :board
+attr_accessor :pos
+
+MOVE_DELTA = [
+  [1,1],
+  [-1,1]
+]
+
+  def initialize(color, pos, board)
+    @king = false
     @color = color #raise exception if not black or white
     @pos = pos # raise exception if not on board
     @board = board
-    board.add_piece(self, pos)
+    board.add_piece(self, @pos)
   end
 
-  move_diffs
+  def king?
+    @king
+  end
+
+  def move_diffs
+    moves = []
+    x = self.pos[0]
+    y = self.pos[1]
+
+    if self.color == :l
+      MOVE_DELTA.each do |d_x, d_y|
+        new_pos = [d_x + x, d_y + y]
+        moves << [d_x,d_y] if self.board.valid_pos?(new_pos)
+      end
+    else
+      MOVE_DELTA.each do |d_x, d_y|
+        new_pos = [d_x + x, y - d_y]
+        moves << [d_x,(-d_y)] if self.board.valid_pos?(new_pos)
+      end
+    end
+    moves
+  end
+
+  def perform_slide(target)
+    new_moves = self.move_diffs.collect{|d_x, d_y| [d_x + pos[0], d_y + pos[1]]}
+    return false unless new_moves.include?(target) && board[target].nil?
+      self.board[pos] = nil
+      self.board[target] = self
+      self.pos = target
+
+    true
+  end
+
+  def perform_jump(target)
+    jump_moves = self.move_diffs.collect do|d_x, d_y|
+      jump_move = [(d_x * 2) + pos[0], (d_y * 2) + pos[1]]
+      mid_move = [d_x + pos[0], d_y + pos[1]]
+      next unless self.board.valid_pos?(jump_move) && !self.board[mid_move].nil? &&
+      self.board[mid_move].color != self.color
+      jump_move
+    end.compact
+
+    return false unless jump_moves.include?(target) && board[target].nil?
+
+    target_mid_move = self.mid_move(target)
+
+    self.board[pos] = nil
+    self.board[target_mid_move] = nil
+    self.board[target] = self
+    self.pos = target
+
+    true
+  end
+
+  def mid_move(target)
+    d_x = (target[0] - self.pos[0])/2
+    d_y = (target[1] - self.pos[1])/2
+
+    mid_move = [d_x + self.pos[0], d_y + self.pos[1]]
+  end
+
+
+
 
 
 end
